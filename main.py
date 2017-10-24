91a3654127b0253d7f3ca54e41528417ca1b2d31
Rutuja Wanjari <rutuja.wanjari2@gmail.com>
2017-10-24 16:49:14 +0530

    url change

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from project import app
 import time
 
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 33507))
-#     host = 'http://instarem-bot.herokuapp.com/'
-#     app.run(debug=True, port=port, host= host)
-
-
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 8081))
-   app.run(debug=True, port=port, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 33507))
+    host = 'http://stressbot.herokuapp.com/'
+    app.run(debug=True, port=port, host= host)
