3c84b6142e8e93f05b43e6a5276ae3e475c92f20
Rutuja Wanjari <rutuja.wanjari2@gmail.com>
2017-10-24 16:15:48 +0530

    message send-receive

--- project/controllers/nlp.py
+++ project/controllers/nlp.py
@@ -1,13 +1,16 @@
 import urllib
 from urllib import parse, request
+import json
 
 
-try:
-    text = urllib.parse.urlencode({'text': 'I am good'})
-    text = text.encode('UTF-8')
-    url = urllib.request.Request('http://text-processing.com/api/sentiment/', text)
-    url.add_header("Content-Type", "application/x-www-form-urlencoded")
-    responseData = urllib.request.urlopen(url).read().decode('utf8', 'ignore')
-    print(responseData)
-except Exception as e:
-    print(str(e))
+def bot_response(message):
+    try:
+        text = urllib.parse.urlencode({'text': message})
+        text = text.encode('UTF-8')
+        url = urllib.request.Request('http://text-processing.com/api/sentiment/', text)
+        url.add_header("Content-Type", "application/x-www-form-urlencoded")
+        response = urllib.request.urlopen(url).read().decode('utf8', 'ignore')
+        response = json.loads(response)
+        return response['label']
+    except Exception as e:
+        return "Sorry, I didn't get that"
